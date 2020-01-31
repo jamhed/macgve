e5b0f5985cece93464c0c2ae255c0a4999413ad9
Roman Galeev <jamhedd@gmail.com>
2020-02-01 00:23:54 +0100

    More sane way to provide custom certificates

--- src/mutate/insert.go
+++ src/mutate/insert.go
@@ -6,10 +6,8 @@ func mutateContainer(c corev1.Container, gve *GoVaultEnv) corev1.Container {
 	args := append(c.Command, c.Args...)
 	c.Command = []string{"/vault/govaultenv", "-addr=" + gve.vaultaddr, "-kubeauth=" + gve.authpath, "-stripname=true"}
 	c.Args = args
-	c.VolumeMounts = append(c.VolumeMounts, []corev1.VolumeMount{
-		{Name: "govaultenv", MountPath: "/vault"},
-		{Name: "tls-certs", MountPath: "/etc/ssl"},
-	}...)
+	c.VolumeMounts = append(c.VolumeMounts, corev1.VolumeMount{Name: "govaultenv", MountPath: "/vault"})
+	c.Env = append(c.Env, corev1.EnvVar{Name: "SSL_CERT_FILE", Value: "/vault/ca-certificates.crt"})
 	return c
 }
 
@@ -20,21 +18,11 @@ func insertGve(pod *corev1.Pod, gve *GoVaultEnv) *corev1.Pod {
 		}
 	}
 	pod.Spec.InitContainers = append(pod.Spec.InitContainers, makeInitContainer(gve.image))
-	pod.Spec.Volumes = append(pod.Spec.Volumes, makeVolumes()...)
+	pod.Spec.Volumes = append(pod.Spec.Volumes, corev1.Volume{
+		Name:         "govaultenv",
+		VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{Medium: corev1.StorageMediumMemory}},
+	})
 	return pod
-}
-
-func makeVolumes() []corev1.Volume {
-	return []corev1.Volume{
-		{
-			Name:         "govaultenv",
-			VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{Medium: corev1.StorageMediumMemory}},
-		},
-		{
-			Name:         "tls-certs",
-			VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{Medium: corev1.StorageMediumMemory}},
-		},
-	}
 }
 
 func makeInitContainer(gveImage string) corev1.Container {
@@ -42,10 +30,7 @@ func makeInitContainer(gveImage string) corev1.Container {
 		Name:            "govaultenv-init",
 		Image:           gveImage,
 		ImagePullPolicy: corev1.PullIfNotPresent,
-		Command:         []string{"sh", "-c", "cp govaultenv /vault/ && cp ca-certificates.crt /etc/ssl/cert.pem"},
-		VolumeMounts: []corev1.VolumeMount{
-			{Name: "govaultenv", MountPath: "/vault"},
-			{Name: "tls-certs", MountPath: "/etc/ssl"},
-		},
+		Command:         []string{"sh", "-c", "cp govaultenv /vault/ && cp ca-certificates.crt /vault/ca-certificates.crt"},
+		VolumeMounts:    []corev1.VolumeMount{{Name: "govaultenv", MountPath: "/vault"}},
 	}
 }
