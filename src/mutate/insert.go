6418e9b301742c6ceb185b827e5bfb7ece539dac
Roman Galeev <jamhedd@gmail.com>
2020-01-31 08:38:33 +0100

    Copy and expose trusted certs

--- src/mutate/insert.go
+++ src/mutate/insert.go
@@ -6,7 +6,10 @@ func mutateContainer(c corev1.Container, gve *GoVaultEnv) corev1.Container {
 	args := append(c.Command, c.Args...)
 	c.Command = []string{"/vault/govaultenv", "-addr=" + gve.vaultaddr, "-kubeauth=" + gve.authpath, "-stripname=true"}
 	c.Args = args
-	c.VolumeMounts = append(c.VolumeMounts, corev1.VolumeMount{Name: "govaultenv", MountPath: "/vault"})
+	c.VolumeMounts = append(c.VolumeMounts, []corev1.VolumeMount{
+		{Name: "govaultenv", MountPath: "/vault"},
+		{Name: "tls-certs", MountPath: "/etc/ssl"},
+	}...)
 	return c
 }
 
@@ -17,17 +20,19 @@ func insertGve(pod *corev1.Pod, gve *GoVaultEnv) *corev1.Pod {
 		}
 	}
 	pod.Spec.InitContainers = append(pod.Spec.InitContainers, makeInitContainer(gve.image))
-	pod.Spec.Volumes = append(pod.Spec.Volumes, makeVolume())
+	pod.Spec.Volumes = append(pod.Spec.Volumes, makeVolumes()...)
 	return pod
 }
 
-func makeVolume() corev1.Volume {
-	return corev1.Volume{
-		Name: "govaultenv",
-		VolumeSource: corev1.VolumeSource{
-			EmptyDir: &corev1.EmptyDirVolumeSource{
-				Medium: corev1.StorageMediumMemory,
-			},
+func makeVolumes() []corev1.Volume {
+	return []corev1.Volume{
+		{
+			Name:         "govaultenv",
+			VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{Medium: corev1.StorageMediumMemory}},
+		},
+		{
+			Name:         "tls-certs",
+			VolumeSource: corev1.VolumeSource{EmptyDir: &corev1.EmptyDirVolumeSource{Medium: corev1.StorageMediumMemory}},
 		},
 	}
 }
@@ -37,12 +42,10 @@ func makeInitContainer(gveImage string) corev1.Container {
 		Name:            "govaultenv-init",
 		Image:           gveImage,
 		ImagePullPolicy: corev1.PullIfNotPresent,
-		Command:         []string{"sh", "-c", "cp /govaultenv /vault/"},
+		Command:         []string{"sh", "-c", "cp /govaultenv /vault/ && cp /ca-certificates.crt /etc/ssl/cert.pem"},
 		VolumeMounts: []corev1.VolumeMount{
-			{
-				Name:      "govaultenv",
-				MountPath: "/vault",
-			},
+			{Name: "govaultenv", MountPath: "/vault"},
+			{Name: "tls-certs", MountPath: "/etc/ssl"},
 		},
 	}
 }
