f08bd4d0ad71d3fe71110a66275f96cf9e50485e
Roman Galeev <jamhedd@gmail.com>
2022-03-04 23:31:55 +0100

    Fix bumper

--- bump-chart.sh
+++ bump-chart.sh
@@ -2,6 +2,6 @@
 CHART=$1
 VERSION=$(yq .version $CHART)
 MINOR=$(echo $VERSION | sed -E 's|^.*\.([0-9]+)$|\1|')
-MAJOR=$(echo $VERSION | sed -E 's|^v(.*)\.[0-9]+$|\1|')
+MAJOR=$(echo $VERSION | sed -E 's|^(.*)\.[0-9]+$|\1|')
 NEWVER="$MAJOR.$((MINOR+1))"
 yq -i '.version=env(NEWVER)' $CHART
