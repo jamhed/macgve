8ea0e3990e42348d5abe7ca9b1895b858c217225
Roman Galeev <jamhedd@gmail.com>
2022-03-05 10:42:34 +0100

    Add default parameter to bump-chart

--- bump-chart.sh
+++ bump-chart.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -ae
-CHART=$1
+CHART=${1:-"charts/macgve/Chart.yaml"}
 VERSION=$(yq .version $CHART)
 MINOR=$(echo $VERSION | sed -E 's|^.*\.([0-9]+)$|\1|')
 MAJOR=$(echo $VERSION | sed -E 's|^(.*)\.[0-9]+$|\1|')
