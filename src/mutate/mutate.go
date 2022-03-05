fda79105a2e9aa75adcdf6caa61585cec575394c
Roman Galeev <jamhedd@gmail.com>
2022-03-05 02:25:35 +0100

    Switch to v1 version

--- src/mutate/mutate.go
+++ src/mutate/mutate.go
@@ -5,7 +5,7 @@ import (
 
 	log "github.com/sirupsen/logrus"
 	jsonpatch "gomodules.xyz/jsonpatch/v2"
-	"k8s.io/api/admission/v1beta1"
+	"k8s.io/api/admission/v1"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
@@ -53,23 +53,23 @@ func mutable(pod *corev1.Pod) *GoVaultEnv {
 	return gve
 }
 
-func Mutate(ar *v1beta1.AdmissionReview, vaultaddr, gveimage string) *v1beta1.AdmissionResponse {
+func Mutate(ar *v1.AdmissionReview, vaultaddr, gveimage string) *v1.AdmissionResponse {
 	var pod *corev1.Pod
 	req := ar.Request
 	log.Infof("Mutate kind:%s namespace:%s, name:%s, op:%s, userinfo:%s, uid:%s", req.Kind, req.Namespace, req.Name, req.Operation, req.UserInfo, req.UID)
 
 	if req.Kind.Kind != "Pod" {
-		return &v1beta1.AdmissionResponse{Allowed: true}
+		return &v1.AdmissionResponse{Allowed: true}
 	}
 
 	if err := json.Unmarshal(req.Object.Raw, &pod); err != nil {
 		log.Errorf("Could not unmarshal raw object: %v", err)
-		return &v1beta1.AdmissionResponse{Result: &metav1.Status{Message: err.Error()}}
+		return &v1.AdmissionResponse{Result: &metav1.Status{Message: err.Error()}}
 	}
 
 	gve := mutable(pod)
 	if gve == nil {
-		return &v1beta1.AdmissionResponse{Allowed: true}
+		return &v1.AdmissionResponse{Allowed: true}
 	}
 
 	origin := pod.DeepCopy()
@@ -80,20 +80,20 @@ func Mutate(ar *v1beta1.AdmissionReview, vaultaddr, gveimage string) *v1beta1.Ad
 	patch, err := makePatch(origin, pod)
 	if err != nil {
 		log.Errorf("Could not make patch, error: %v", err)
-		return &v1beta1.AdmissionResponse{Result: &metav1.Status{Message: err.Error()}}
+		return &v1.AdmissionResponse{Result: &metav1.Status{Message: err.Error()}}
 	}
 	bytes, err := json.Marshal(patch)
 	if err != nil {
 		log.Errorf("Could not marshal patch object: %v", err)
-		return &v1beta1.AdmissionResponse{Result: &metav1.Status{Message: err.Error()}}
+		return &v1.AdmissionResponse{Result: &metav1.Status{Message: err.Error()}}
 	}
 	log.Debugf("Mutated response: %s", string(bytes))
 
-	return &v1beta1.AdmissionResponse{
+	return &v1.AdmissionResponse{
 		Allowed: true,
 		Patch:   bytes,
-		PatchType: func() *v1beta1.PatchType {
-			pt := v1beta1.PatchTypeJSONPatch
+		PatchType: func() *v1.PatchType {
+			pt := v1.PatchTypeJSONPatch
 			return &pt
 		}(),
 	}
